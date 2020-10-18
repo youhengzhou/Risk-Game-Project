61eb20354b473083f04d4bc598b18c7b1fd4dc1c
iambruceliu <2219455084@qq.com>
2020-10-18 18:01:53 -0400

    test2

--- src/Battle.java
+++ src/Battle.java
@@ -46,6 +46,7 @@ public class Battle {
         Country canada = new Country( "canada");
         Country China = new Country( "china");
         System.out.println("test");
+        System.out.println("test2");
         System.out.print(new Battle(China, canada).fight());
     }
 }

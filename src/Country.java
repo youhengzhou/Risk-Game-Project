628d4ce9b72142a9dad24a351224a794168dc8ee
iambruceliu <2219455084@qq.com>
2020-10-18 15:48:36 -0400

    implemented Changeowner,addadjacentcountry,printstate

--- src/Country.java
+++ src/Country.java
@@ -17,6 +17,7 @@ public class Country {
     private ArrayList<Country> adjacentCountries;
 
     public Country(int troopsnum, Player player,String name){
+        //initializing troopnum,owner and countryName
         troopsNum=troopsnum;
         owner=player;
         countryName=name;
@@ -25,18 +26,42 @@ public class Country {
     }
     public void addtroops(int num)
     {
+        //add new troops to the country
         troopsNum+=num;
     }
 
     public void removetroop(int num)
     {
+        //remove troops in the country
         troopsNum-=num;
     }
 
     public int getTroopsNum()
     {
+        //return total number of troops in the conutry
         return troopsNum;
     }
 
+    public void changeOwner(Player newowner)
+    {
+        //change owner of the country
+        owner.removeCountry(this);
+        this.owner=newowner;
+    }
 
+    public void addAdjacentCountry(Country adc)
+    {
+        //add adjacent countries to the AdjacentCountries list
+        adjacentCountries.add(adc);
+    }
+
+    public String printState() {
+        //print the country state
+        return "Country{" +
+                "countryName='" + countryName + '\'' +
+                ", owner=" + owner +
+                ", troopsNum=" + troopsNum +
+                ", adjacentCountries=" + adjacentCountries +
+                '}';
+    }
 }

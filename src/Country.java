c097e767e68a8a7fb880c93e8b86460e0bb453eb
youhengzhou <youhengzhou@cmail.carleton.ca>
2020-10-17 23:38:35 -0400

    Added Country

---
+++ src/Country.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Country {
+    private String countryName;
+    private String owner;
+    private int troopsNum;
+    private ArrayList<Country> adjacentCountries;
+
+    private Country(){
+
+    }
+
+}

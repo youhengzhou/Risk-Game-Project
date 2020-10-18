9aeb13432399a1add82afa41b58ffeb2fc459e52
iambruceliu <2219455084@qq.com>
2020-10-18 17:53:19 -0400

    Add countries and some adjacent countries

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,88 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    List<Player> players;
+    List<Country> countries;
+    boolean hasWinner;
+    Player playerOnGoing;
+
+    public Game()
+    {
+        players= new ArrayList<>();
+        countries = new ArrayList<>();
+        hasWinner = false;
+
+    }
+
+    public void initCountries()
+    {
+        //North America
+        Country Alaska = new Country("Alaska");
+        Country Alberta = new Country("Alberta");
+        Country CentralAmerica = new Country("Central America");
+        Country EasternUnitedStates = new Country("Eastern United States");
+        Country Greenland = new Country("Greenland");
+        Country NorthwestTerritory = new Country("Northwest Territory");
+        Country Ontario = new Country("Ontario");
+        Country Quebec = new Country("Quebec");
+        Country WesternUnitedStates = new Country("WesterUnitedState");
+
+        //South America
+        Country Argentina = new Country("Argentina");
+        Country Brazil = new Country("Brazil");
+        Country Peru = new Country("Peru");
+        Country Venezuela = new Country("Venezuela");
+
+        //Europe
+        Country GreatBritain = new Country("Great Britain");
+        Country Iceland = new Country("Iceland");
+        Country NorthernEurope = new Country("Northern Europe");
+        Country Scandinavia = new Country("Scandinavia");
+        Country SouthernEurope = new Country("Southern Europe");
+        Country Ukraine= new Country("Ukraine");
+        Country WesternEurope = new Country("Western Europe");
+
+        //Africa
+        Country Congo = new Country("Congo");
+        Country EastAfrica = new Country("East Africa");
+        Country Egypt = new Country("Egypt");
+        Country Madagascar = new Country("Madagascar");
+        Country NorthAfrica = new Country("North Africa");
+        Country SouthAfrica = new Country("South Africa");
+
+        //Asia
+        Country Afghanistan = new Country("Afghanistan");
+        Country China = new Country("China");
+        Country India = new Country("India");
+        Country Irkutsk = new Country("Irkutsk");
+        Country Japan = new Country("Japan");
+        Country Kamchatka = new Country("Kamchatka");
+        Country MiddleEast = new Country("Middle East");
+        Country Mongolia = new Country("Mongolia");
+        Country Siam  = new Country("Siam");
+        Country Siberia = new Country("Siberia");
+        Country Ural = new Country("Ural");
+        Country Yakutsk = new Country("Yakutsk");
+
+        //Australia
+        Country EasternAustralia = new Country("Eastern Australia");
+        Country Indonesia = new Country("Indonesia");
+        Country NewGuinea = new Country("New Guinea");
+        Country WesternAustralia = new Country("Western Australia");
+
+        //Add adjacent Countries
+        //North America
+        Alaska.addAdjacentCountry(NorthwestTerritory);
+        Alaska.addAdjacentCountry(Kamchatka);
+        Alberta.addAdjacentCountry(NorthwestTerritory);
+        Alberta.addAdjacentCountry(Ontario);
+        Alberta.addAdjacentCountry(WesternUnitedStates);
+        CentralAmerica.addAdjacentCountry(EasternUnitedStates);
+        CentralAmerica.addAdjacentCountry(WesternUnitedStates);
+        CentralAmerica.addAdjacentCountry(Argentina);
+
+
+
+    }
 }

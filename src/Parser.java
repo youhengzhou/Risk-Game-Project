9873f21951c14436167b0aa88d9efec5950a6693
You Heng Zhou <60205850+youhengzhou@users.noreply.github.com>
2020-10-19 16:35:05 -0400

    Parser Initial file

---
+++ src/Parser.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Parser 
+{
+    private CommandWords commands;  // holds all valid command words
+    private Scanner reader;         // source of command input
+
+    /**
+     * Create a parser to read from the terminal window.
+     */
+    public Parser() 
+    {
+        commands = new CommandWords();
+        reader = new Scanner(System.in);
+    }
+
+    /**
+     * @return The next command from the user.
+     */
+    public Command getCommand() 
+    {
+        String inputLine;   // will hold the full input line
+        String word1 = null;
+        String word2 = null;
+
+        System.out.print("> ");     // print prompt
+
+        inputLine = reader.nextLine();
+
+        // Find up to two words on the line.
+        Scanner tokenizer = new Scanner(inputLine);
+        if(tokenizer.hasNext()) {
+            word1 = tokenizer.next();      // get first word
+            if(tokenizer.hasNext()) {
+                word2 = tokenizer.next();      // get second word
+            }
+        }
+        if(commands.isCommand(word1)) {
+            return new Command(word1, word2);
+        }
+        else {
+            return new Command(null, word2); 
+        }
+    }
+
+    /**
+     * Print out a list of valid command words.
+     */
+    public void showCommands()
+    {
+        commands.showAll();
+    }
+}

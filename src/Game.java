ea430c7f0e94a3cd68e097f6aba29d7975322b28
iambruceliu <2219455084@qq.com>
2020-10-19 22:51:50 -0400

    finn no change

--- src/Game.java
+++ src/Game.java
@@ -1,39 +1,43 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class Game {
+    private HashMap<String, Country> map;
     private List<Player> players;
-    private List<Country> countries;
+    private Scanner reader;
     private boolean hasWinner;
     private Player playerOnGoing;
     private int numOfPlayer = 0;
     private int initialTroops = 0;
-	private Parser parser;
-	private Country currentCountry;
-    public Game() 
+    private Parser parser;
+    private HashMap<String,Country> nameCountryPairs;
+    private CommandWords cwords;
+    public Game()
     {
-		parser = new Parser(); // parser for word checks
+        parser = new Parser(); // parser for word checks
         players= new ArrayList<>();
-        countries = new ArrayList<>();
+        map = new HashMap<>();
         hasWinner = false;
+        nameCountryPairs = new HashMap<>();
 
         setNumOfPlayer();
 
         System.out.println("Let's play the game, you can type 'help' to see how to play this game");
     }
-	
-	public void play() 
-    {            
+
+    public void play()
+    {
         boolean finished = false;
         while (! finished) {
-            Command command = parser.getCommand();
-            finished = processCommand(command);
+
+
         }
         System.out.println("Thank you for playing. Good bye.");
     }
-	
-	private void printWelcome()
+
+    private void printWelcome()
     {
         System.out.println();
         System.out.println("Welcome to the RISK!");
@@ -42,8 +46,8 @@ public class Game {
         System.out.println();
         //System.out.println(currentCountry.getLongDescription()); something that describes the current player situation
     }
-	
-	private boolean processCommand(Command command) 
+
+    private boolean processCommand(Command command)
     {
         boolean wantToQuit = false;
 
@@ -65,50 +69,36 @@ public class Game {
         // else command not recognised.
         return wantToQuit;
     }
-	
-	private void printHelp() 
+
+    private void printHelp()
     {
-        System.out.println("You are a general. You are leading your army to conquer the world!");
-        System.out.println("Ready your armies, for your enemies would be ready for you.");
+        System.out.println("You are a general. You are leading your army to conquer the world");
+        System.out.println("around at the university.");
         System.out.println();
         System.out.println("Your command words are:");
-        parser.showCommands();
+
     }
-	
-	private void attack(Command command)
+
+	/*private void attack(Command command)
     {
         if(!command.hasSecondWord()) {
             // if there is no second word, we don't know where to attack...
             System.out.println("Attack where?");
             return;
         }
-
-        System.out.println("Where are we attacking this from, General?");
-
-        while (! finished) {
-            Command command2 = parser.getCommand();
-            finished = processCommand(command);
+        String direction = command.getSecondWord();
+        // Try to leave current room.
+        Country adjacentCountry = currentCountry.getAdjacentCountry(direction);
+        if (adjacentCountry == null) {
+            System.out.println("There is no adjacent country!");
         }
-
-        String attackCountry = command.getSecondWord(); // the country getting attacked, chosen by the player
-        String currentCountry = command2.getWord(); // the country to attack from
-
-        // Try to attack another country.
-        ArrayList<Country> adjacentCountryList = currentCountry.getAdjacentCountry();
-
-        for(int i = adjacentCountryList.size(); i>=0 ; i--){
-            Country adjacentCountry = adjacentCountryList.get(i);
-            if ((adjacentCountry.getCountryName()).equals(attackCountry)) {
-                if(currentCountry.getTroopsNum() > attackCountry.getTroopsNum()) { // checks if the attacking side has enough dice
-                    System.out.print(new Battle(currentCountry, adjacentCountry).fight());
-                    System.out.println(adjacentCountry.printState());
-                }
-            }
+        else {
+            currentCountry = nextCountry;
+            System.out.println(currentCountry.getLongDescription());
         }
-        System.out.println("This attack is not possible, General!"); // when all the conditions fail, the attack is not possible
-    }
-	
-	private boolean quit(Command command) 
+    }*/
+
+    private boolean quit(Command command)
     {
         if(command.hasSecondWord()) {
             System.out.println("Quit what?");
@@ -134,7 +124,7 @@ public class Game {
         Country NorthwestTerritory = new Country("Northwest Territory");
         Country Ontario = new Country("Ontario");
         Country Quebec = new Country("Quebec");
-        Country WesternUnitedStates = new Country("WesterUnitedState");
+        Country WesternUnitedStates = new Country("WesternUnitedState");
 
         //South America
         Country Argentina = new Country("Argentina");
@@ -181,13 +171,274 @@ public class Game {
 
         //Add adjacent Countries
         //North America
-        Alaska.addAdjacentCountry(NorthwestTerritory);
+        //1
         Alaska.addAdjacentCountry(Kamchatka);
+        Alaska.addAdjacentCountry(Alberta);
+        Alaska.addAdjacentCountry(NorthwestTerritory);
+        //2
+        Alberta.addAdjacentCountry(Alaska);
         Alberta.addAdjacentCountry(NorthwestTerritory);
         Alberta.addAdjacentCountry(Ontario);
         Alberta.addAdjacentCountry(WesternUnitedStates);
+        //3
         CentralAmerica.addAdjacentCountry(EasternUnitedStates);
         CentralAmerica.addAdjacentCountry(WesternUnitedStates);
+        CentralAmerica.addAdjacentCountry(Venezuela);
+        //4
+        EasternUnitedStates.addAdjacentCountry(CentralAmerica);
+        EasternUnitedStates.addAdjacentCountry(Ontario);
+        EasternUnitedStates.addAdjacentCountry(Quebec);
+        EasternUnitedStates.addAdjacentCountry(WesternUnitedStates);
+        //5
+        Greenland.addAdjacentCountry(NorthwestTerritory);
+        Greenland.addAdjacentCountry(Ontario);
+        Greenland.addAdjacentCountry(Quebec);
+        Greenland.addAdjacentCountry(Iceland);
+        //6
+        NorthwestTerritory.addAdjacentCountry(Alaska);
+        NorthwestTerritory.addAdjacentCountry(Alberta);
+        NorthwestTerritory.addAdjacentCountry(Ontario);
+        NorthwestTerritory.addAdjacentCountry(Greenland);
+        //7
+        Ontario.addAdjacentCountry(CentralAmerica);
+        Ontario.addAdjacentCountry(EasternUnitedStates);
+        Ontario.addAdjacentCountry(Greenland);
+        Ontario.addAdjacentCountry(NorthwestTerritory);
+        Ontario.addAdjacentCountry(Quebec);
+        Ontario.addAdjacentCountry(WesternUnitedStates);
+        //8
+        Quebec.addAdjacentCountry(EasternUnitedStates);
+        Quebec.addAdjacentCountry(Greenland);
+        Quebec.addAdjacentCountry(Ontario);
+        //9
+        WesternUnitedStates.addAdjacentCountry(Alberta);
+        WesternUnitedStates.addAdjacentCountry(CentralAmerica);
+        WesternUnitedStates.addAdjacentCountry(EasternUnitedStates);
+        WesternUnitedStates.addAdjacentCountry(Ontario);
+
+        //South America
+        //1
+        Argentina.addAdjacentCountry(Brazil);
+        Argentina.addAdjacentCountry(Peru);
+        //2
+        Brazil.addAdjacentCountry(Argentina);
+        Brazil.addAdjacentCountry(Peru);
+        Brazil.addAdjacentCountry(Venezuela);
+        Brazil.addAdjacentCountry(NorthAfrica);
+        //3
+        Peru.addAdjacentCountry(Argentina);
+        Peru.addAdjacentCountry(Brazil);
+        Peru.addAdjacentCountry(Venezuela);
+        //4
+        Venezuela.addAdjacentCountry(Brazil);
+        Venezuela.addAdjacentCountry(Peru);
+        Venezuela.addAdjacentCountry(CentralAmerica);
+
+        //Europe
+        //1, 2347
+        GreatBritain.addAdjacentCountry(Iceland);
+        GreatBritain.addAdjacentCountry(NorthernEurope);
+        GreatBritain.addAdjacentCountry(Scandinavia);
+        GreatBritain.addAdjacentCountry(WesternEurope);
+        //2, 14  5
+        Iceland.addAdjacentCountry(GreatBritain);
+        Iceland.addAdjacentCountry(Scandinavia);
+        Iceland.addAdjacentCountry(Greenland);
+        //3, 14567
+        NorthernEurope.addAdjacentCountry(GreatBritain);
+        NorthernEurope.addAdjacentCountry(Scandinavia);
+        NorthernEurope.addAdjacentCountry(SouthernEurope);
+        NorthernEurope.addAdjacentCountry(Ukraine);
+        NorthernEurope.addAdjacentCountry(WesternEurope);
+        //4, 1236
+        Scandinavia.addAdjacentCountry(GreatBritain);
+        Scandinavia.addAdjacentCountry(Iceland);
+        Scandinavia.addAdjacentCountry(NorthernEurope);
+        Scandinavia.addAdjacentCountry(Ukraine);
+        //5, 367 35 7
+        SouthernEurope.addAdjacentCountry(NorthernEurope);
+        SouthernEurope.addAdjacentCountry(Ukraine);
+        SouthernEurope.addAdjacentCountry(WesternEurope);
+        SouthernEurope.addAdjacentCountry(Egypt);
+        SouthernEurope.addAdjacentCountry(NorthAfrica);
+        SouthernEurope.addAdjacentCountry(MiddleEast);
+        //6, 345  17 11
+        Ukraine.addAdjacentCountry(NorthernEurope);
+        Ukraine.addAdjacentCountry(Scandinavia);
+        Ukraine.addAdjacentCountry(SouthernEurope);
+        Ukraine.addAdjacentCountry(Afghanistan);
+        Ukraine.addAdjacentCountry(MiddleEast);
+        Ukraine.addAdjacentCountry(Ural);
+        //7, 135 5
+        WesternEurope.addAdjacentCountry(GreatBritain);
+        WesternEurope.addAdjacentCountry(NorthernEurope);
+        WesternEurope.addAdjacentCountry(SouthernEurope);
+        WesternEurope.addAdjacentCountry(NorthAfrica);
+
+        //Africa
+        //1, 256
+        Congo.addAdjacentCountry(EastAfrica);
+        Congo.addAdjacentCountry(NorthAfrica);
+        Congo.addAdjacentCountry(SouthAfrica);
+        //2, 13456 7
+        EastAfrica.addAdjacentCountry(Congo);
+        EastAfrica.addAdjacentCountry(Egypt);
+        EastAfrica.addAdjacentCountry(Madagascar);
+        EastAfrica.addAdjacentCountry(NorthAfrica);
+        EastAfrica.addAdjacentCountry(SouthAfrica);
+        EastAfrica.addAdjacentCountry(MiddleEast);
+        //3, 25 6 7
+        Egypt.addAdjacentCountry(EastAfrica);
+        Egypt.addAdjacentCountry(NorthAfrica);
+        Egypt.addAdjacentCountry(Ukraine);
+        Egypt.addAdjacentCountry(MiddleEast);
+        //4, 26
+        Madagascar.addAdjacentCountry(EastAfrica);
+        Madagascar.addAdjacentCountry(SouthAfrica);
+        //5, 123 67 2
+        NorthAfrica.addAdjacentCountry(Congo);
+        NorthAfrica.addAdjacentCountry(EastAfrica);
+        NorthAfrica.addAdjacentCountry(Egypt);
+        NorthAfrica.addAdjacentCountry(Ukraine);
+        NorthAfrica.addAdjacentCountry(WesternEurope);
+        NorthAfrica.addAdjacentCountry(Brazil);
+        //6, 124
+        SouthAfrica.addAdjacentCountry(Congo);
+        SouthAfrica.addAdjacentCountry(EastAfrica);
+        SouthAfrica.addAdjacentCountry(Madagascar);
+
+        //Asia
+        //1, 237 11 6
+        Afghanistan.addAdjacentCountry(China);
+        Afghanistan.addAdjacentCountry(India);
+        Afghanistan.addAdjacentCountry(MiddleEast);
+        Afghanistan.addAdjacentCountry(Ural);
+        Afghanistan.addAdjacentCountry(Ukraine);
+        //2, 1389 10 11
+        China.addAdjacentCountry(Afghanistan);
+        China.addAdjacentCountry(India);
+        China.addAdjacentCountry(Mongolia);
+        China.addAdjacentCountry(Siam);
+        China.addAdjacentCountry(Siberia);
+        China.addAdjacentCountry(Ural);
+        //3, 1279
+        India.addAdjacentCountry(Afghanistan);
+        India.addAdjacentCountry(China);
+        India.addAdjacentCountry(MiddleEast);
+        India.addAdjacentCountry(Siam);
+        //4, 68 10 12
+        Irkutsk.addAdjacentCountry(Kamchatka);
+        Irkutsk.addAdjacentCountry(Mongolia);
+        Irkutsk.addAdjacentCountry(Siberia);
+        Irkutsk.addAdjacentCountry(Yakutsk);
+        //5, 68
+        Japan.addAdjacentCountry(Kamchatka);
+        Japan.addAdjacentCountry(Mongolia);
+        //6, 458 12 1
+        Kamchatka.addAdjacentCountry(Irkutsk);
+        Kamchatka.addAdjacentCountry(Japan);
+        Kamchatka.addAdjacentCountry(Mongolia);
+        Kamchatka.addAdjacentCountry(Yakutsk);
+        Kamchatka.addAdjacentCountry(Alaska);
+        //7, 13 56 23
+        MiddleEast.addAdjacentCountry(Afghanistan);
+        MiddleEast.addAdjacentCountry(India);
+        MiddleEast.addAdjacentCountry(SouthernEurope);
+        MiddleEast.addAdjacentCountry(Ukraine);
+        MiddleEast.addAdjacentCountry(EastAfrica);
+        MiddleEast.addAdjacentCountry(Egypt);
+        //8, 2456 10
+        Mongolia.addAdjacentCountry(China);
+        Mongolia.addAdjacentCountry(Irkutsk);
+        Mongolia.addAdjacentCountry(Japan);
+        Mongolia.addAdjacentCountry(Kamchatka);
+        Mongolia.addAdjacentCountry(Siberia);
+        //9, 23 2
+        Siam.addAdjacentCountry(China);
+        Siam.addAdjacentCountry(India);
+        Siam.addAdjacentCountry(Indonesia);
+        //10, 248 11 12
+        Siberia.addAdjacentCountry(China);
+        Siberia.addAdjacentCountry(Irkutsk);
+        Siberia.addAdjacentCountry(Mongolia);
+        Siberia.addAdjacentCountry(Ural);
+        Siberia.addAdjacentCountry(Yakutsk);
+        //11, 12 10 6
+        Ural.addAdjacentCountry(Yakutsk);
+        Ural.addAdjacentCountry(Siberia);
+        Ural.addAdjacentCountry(Ukraine);
+        //12, 46 10
+        Yakutsk.addAdjacentCountry(Irkutsk);
+        Yakutsk.addAdjacentCountry(Kamchatka);
+        Yakutsk.addAdjacentCountry(Siberia);
+
+        //Australia
+        //1, 34
+        EasternAustralia.addAdjacentCountry(NewGuinea);
+        EasternAustralia.addAdjacentCountry(WesternAustralia);
+        //2, 34 9
+        Indonesia.addAdjacentCountry(NewGuinea);
+        Indonesia.addAdjacentCountry(WesternAustralia);
+        Indonesia.addAdjacentCountry(Siam);
+        //3, 124
+        NewGuinea.addAdjacentCountry(EasternAustralia);
+        NewGuinea.addAdjacentCountry(Indonesia);
+        NewGuinea.addAdjacentCountry(WesternAustralia);
+        //4, 12
+        WesternAustralia.addAdjacentCountry(EasternAustralia);
+        WesternAustralia.addAdjacentCountry(Indonesia);
+
+        map.put("alberta",Alberta );
+        map.put("centralamerica ",CentralAmerica  );
+        map.put("easternunitedstates ",EasternUnitedStates  );
+        map.put("greenland ",Greenland  );
+        map.put("northwestterritory ",NorthwestTerritory  );
+        map.put("ontario ",Ontario  );
+        map.put("quebec ",Quebec  );
+        map.put("westernunitedstates ",WesternUnitedStates  );
+
+        map.put("argentina ",Argentina  );
+        map.put("brazil ",Brazil  );
+        map.put("peru ",Peru  );
+        map.put("venezuela ",Venezuela  );
+
+        map.put("greatbritain ",GreatBritain  );
+        map.put("iceland ",Iceland);
+        map.put("northerneurope ",NorthernEurope  );
+        map.put("scandinavia ",Scandinavia  );
+        map.put("southerneurope ",SouthernEurope  );
+        map.put("ukraine",Ukraine );
+        map.put("westerneurope ",WesternEurope);
+
+        map.put("congo",Congo );
+        map.put("eastafrica",EastAfrica );
+        map.put("egypt",Egypt );
+        map.put("madagascar",Madagascar );
+        map.put("northafrica",NorthAfrica );
+        map.put("southafrica",SouthAfrica );
+
+        map.put("afghanistan",Afghanistan );
+        map.put("china",China );
+        map.put("india  ",India );
+        map.put("irkutsk",Irkutsk  );
+        map.put("japan",Japan  );
+        map.put("kamchatka",Kamchatka  );
+        map.put("middleEast",MiddleEast  );
+        map.put("mongolia",Mongolia  );
+        map.put("siam",Siam   );
+        map.put("siberia",Siberia  );
+        map.put("ural",Ural  );
+        map.put("yakutsk",Yakutsk);
+
+        map.put("easternaustralia",EasternAustralia);
+        map.put("indonesia",Indonesia  );
+        map.put("newGuinea",NewGuinea  );
+        map.put("westernAustralia",WesternAustralia  );
+    }
+
+
+
+
     }
 
     private boolean isValidNum(int num){
@@ -220,5 +471,21 @@ public class Game {
             case 6:
                 this.initialTroops = 20;
         }
+    }
+
+    public String getCommandWord()
+    {
+        String commandinput = new String();
+        do{
+            System.out.println("please enter \" Attack\" or \"Move\"");
+            reader = new Scanner(System.in);
+            String str = reader.nextLine();
+            commandinput =str.replace(" ","");}while (!cwords.isCommand(commandinput));
+        return commandinput;
+    }
+
+    public Country getCountryName()
+    {
+        return null;
     }
 }

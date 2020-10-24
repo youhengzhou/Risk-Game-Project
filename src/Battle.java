57a7b898823271ae33dd5d813b1a45a918acc2ee
youhengzhou <youhengzhou@cmail.carleton.ca>
2020-10-24 18:29:06 -0400

    Battle edits

--- src/Battle.java
+++ src/Battle.java
@@ -3,7 +3,7 @@ public class Battle {
     private Country countryDefend;
     private int AttackingTroops;
 
-    public Battle(Country attack, Country defend,int troopSent){
+    public Battle(Country attack, Country defend,int troopSent){ // initialize battle
         this.countryAttack = attack;
         this.countryDefend = defend;
         AttackingTroops=troopSent;
@@ -14,49 +14,38 @@ public class Battle {
     public Boolean fight() {
         int attackTroopDeath = 0;
         int defendTroopDeath=0;
+        Dice attackDice = new Dice(1);
+        Dice defendDice=new Dice(1) ;
 
-            Dice attackDice = new Dice(1);
-            Dice defendDice=new Dice(1) ;
+        while(AttackingTroops!=0 && countryDefend.getTroopsNum()!=0) {
+            attackDice = new Dice(AttackingTroops);
+            defendDice = new Dice(countryDefend.getTroopsNum());
 
-
-            while(AttackingTroops!=0 && countryDefend.getTroopsNum()!=0) {
-                attackDice = new Dice(AttackingTroops);
-                defendDice = new Dice(countryDefend.getTroopsNum());
-
-
-                while (!attackDice.isempty() && !defendDice.isempty()) {
-
-                    int attacknum = attackDice.getNexdtHighest();
-                    int defendnum = defendDice.getNexdtHighest();
-
-
-                        if (attacknum > defendnum) {
-                        countryDefend.removetroop(1);
-
+            while (!attackDice.isempty() && !defendDice.isempty()) {
+                int attacknum = attackDice.getNexdtHighest();
+                int defendnum = defendDice.getNexdtHighest();
+                if (attacknum > defendnum) {
+                    countryDefend.removetroop(1);
                         defendTroopDeath++;
-                    } else {
-                        AttackingTroops--;
-                        attackTroopDeath++;
-                    }
-
+                } else {
+                    AttackingTroops--;
+                    attackTroopDeath++;
                 }
             }
-            System.out.println("\nBattle Result:");
-            System.out.println(countryAttack.getCountryName()+" lost "+attackTroopDeath+" troops in this battle");
-            System.out.println(countryDefend.getCountryName() +" lost "+defendTroopDeath+" troops in this battle");
-            if (countryDefend.getTroopsNum() == 0)
-            {
-
-                int troopSurvive =AttackingTroops;
-                countryDefend.addtroops(troopSurvive);
-                countryDefend.getOwner().removeCountry(countryDefend); //remove the country from the country defender countryOwner
-                countryDefend.changeOwner(countryAttack.getOwner());//change owner of the defendCountry
-                countryAttack.getOwner().addCountry(countryDefend);//add CountryDefend into the attaker CountryOwner
-                System.out.println("You win! now "+countryDefend.getCountryName()+"is yours");
-                System.out.println("Survived "+troopSurvive+" troops moved from "+countryAttack.getCountryName()+" to "+countryDefend.getCountryName()+"\n\n");
-                return true;
-            }
-
+        }
+        System.out.println("\nBattle Result:");
+        System.out.println(countryAttack.getCountryName()+" lost "+attackTroopDeath+" troops in this battle");
+        System.out.println(countryDefend.getCountryName() +" lost "+defendTroopDeath+" troops in this battle");
+        if (countryDefend.getTroopsNum() == 0) {
+            int troopSurvive =AttackingTroops;
+            countryDefend.addtroops(troopSurvive);
+            countryDefend.getOwner().removeCountry(countryDefend); //remove the country from the country defender countryOwner
+            countryDefend.changeOwner(countryAttack.getOwner());//change owner of the defendCountry
+            countryAttack.getOwner().addCountry(countryDefend);//add CountryDefend into the attaker CountryOwner
+            System.out.println("You win! Now "+countryDefend.getCountryName()+"is yours.");
+            System.out.println("Surviving "+troopSurvive+" troops has moved from "+countryAttack.getCountryName()+" to "+countryDefend.getCountryName()+"\n\n");
+            return true;
+        }
         System.out.println("Unfortunately you lose the battle with "+countryDefend.getCountryName());
         return false;
     }

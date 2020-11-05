abf9d02d8190ad8289f2022b75b07c539253511f
Shizhong-Shang <72275111+Shizhong-Shang@users.noreply.github.com>
2020-11-05 03:53:55 -0500

    Update Country.java
    change

--- src/Country.java
+++ src/Country.java
@@ -1,87 +1,134 @@
 import java.util.ArrayList;
 
+/**
+ * the Country class is used to store information of Country instance
+ *
+ * @auther Avengers
+ * @version 1.0
+ * @since  2020-10-25
+ *
+ */
 public class Country {
     private String countryName;
     private Player owner;
     private int troopsNum;
     private ArrayList<Country> adjacentCountries;
 
-    public Country(String name){
-        //initializing troopnum,owner and countryName
-        countryName=name;
+    /**
+     * Constructor for Country,initializing troopsNum,owner and countryName
+     */
+    public Country(String name) {
+
+        countryName = name;
         adjacentCountries = new ArrayList<>();
-        owner=null;
+        owner = null;
 
     }
-    public void addtroops(int num)
-    {
-        //add new troops to the country
-        troopsNum+=num;
+
+    /**
+     * add troops to the country
+     * @param num the number of troops to be add.
+     */
+    public void addTroops(int num) {
+        troopsNum += num;
     }
 
-    public void removetroop(int num) {
-        //remove troops in the country
+    /**
+     * Remove troops from the country.
+     * @param num number of troops to be removed
+     */
+    public void removeTroops(int num) {
+
         troopsNum -= num;
     }
 
 
+    /**
+     * get number of troops on the country
+     * @return troopsNum number of troops on the country
+     */
     public int getTroopsNum() {
-        //return total number of troops in the conutry
-        return troopsNum;
+          return troopsNum;
     }
 
+    /**
+     * get the owner of the country
+     * @return  the owner of the country
+     */
     public Player getOwner() {
+
         return owner;
     }
 
+    /**
+     * get the name of the country
+     * @return countryName name of the country
+     */
     public String getCountryName() {
-        //return name of Country
+
         return countryName;
     }
 
-    public void changeOwner(Player newowner) {
+    /**
+     * Change the owner of the country
+     * @param newOwner the new owner of the country
+     */
+    public void changeOwner(Player newOwner) {
         if (owner == null) {
-            owner = newowner;
+            owner = newOwner;
             return;
         }
-        //change owner of the country
         owner.removeCountry(this);
-        this.owner = newowner;
+        this.owner = newOwner;
     }
 
+    /**
+     * add adjacent countries to the AdjacentCountries list
+     * @param adc the adjacent country to be add.
+     */
     public void addAdjacentCountry(Country adc) {
-        //add adjacent countries to the AdjacentCountries list
+
         adjacentCountries.add(adc);
     }
 
+    /**
+     * get the list of the adjacent countries
+     * @return adjacentCountries Arraylist of the adjacent counties.
+     */
     public ArrayList<Country> getAdjacentCountries() {
+
         return adjacentCountries;
     }
 
+    /**
+     * get the String of troops on this country
+     * @return  the String of troops on this country
+     */
     public String printState() {
-        //print name and troops
+        // print name and troops
         String s = "";
         s += countryName + " (" + troopsNum + " troops)";
         return s;
     }
 
-    public void printAdjacentCountries() {
+    /**
+     * get the string of adjacent enemy countries.
+     * @return a string of adjacent enemy countries.
+     */
+    public String printEnemyCountry() {
         String s = "";
         for (Country c : this.getAdjacentCountries()) {
-            s += c.printState();
-            s += "\n";
-        }
-        System.out.println(s);
-    }
-    public String printEnemyCountry()
-    {
-        String s = "";
-        for (Country c : this.getAdjacentCountries()) {
-            if(c.getOwner().equals(this.getOwner())) continue;
+
+
+            if (c.getOwner().equals(this.getOwner())) continue;
             s += c.printState();
             s += "\n";
         }
         return s;
+    }
 
+    @Override
+    public String toString() {
+        return countryName + "(troops: "+ troopsNum + " )\n";
     }
 }

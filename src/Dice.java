0d96b08aa8d9e9831d2c74fdc8a76a2e4318c073
Shizhong-Shang <72275111+Shizhong-Shang@users.noreply.github.com>
2020-11-05 03:57:50 -0500

    Update Dice.java
    change

--- src/Dice.java
+++ src/Dice.java
@@ -1,59 +1,50 @@
-import java.util.Collection;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
-/**1st Edition Youheng Zhou
- * Oct 17th
- * Implemented Dice(),roll()
+/**
+ * the Dice class is used for random number generations and putting numbers in integer queues for multiple dice rolls
  *
- * 2nd Edition Shaopeng Liu
- * Oct 18th
- * Modifed Dice(), roll()
- * Implemented getNextHighest(), isempty(),
+ * @auther Avengers
+ * @version 1.0
+ * @since  2020-10-25
+ *
  */
-
 public class Dice {
 
     private PriorityQueue<Integer> diceGroup;
-    public Dice(int diceNumber){
+
+    /**
+    * @ param diceNumber in the diceGroup one by one
+    */
+    public Dice(int diceNumber) {
         //store values in the diceGroup,number of Integer added is depending on the diceNumber parameter passed in.
-        diceGroup = new PriorityQueue<Integer>(Collections.reverseOrder());
-        for(int i=0;i<diceNumber;i++)
-        {
+        diceGroup = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < diceNumber; i++) {
             diceGroup.add(roll());
         }
     }
 
-    public int roll()
-    {
-        //generate a random value from 1 to 6 inclusive.
-        return (int) Math.ceil(Math.random()*6);
+    /**
+    * @ return to a random value from 1 to 6
+    */
+    public int roll() {
+        return (int) Math.ceil(Math.random() * 6);
     }
 
-    public int getNexdtHighest()
-    {
-        //get the next highest value in queue, if no more values in queue, throw NullPointerException.
-        if(isempty()) throw (new NullPointerException("no more dice"));
+    /**
+    * get the next highest value in queue, if no more values in queue, throw NullPointerException
+    * @ return diceGroup.isEmpty()
+    */
+    public int getNextHighest() {
+        if (isEmpty()) throw (new NullPointerException("no more dice"));
         return diceGroup.poll();
     }
 
-    public boolean isempty()
-    {
-        //test if the diceGroup is empty
+    /**
+    * test if the diceGroup is empty
+    * @ reutrn diceGroup.isEmpty()
+    */
+    public boolean isEmpty() {
         return diceGroup.isEmpty();
-    }
-
-    //to make sure the attackDice has one Troop left
-    public boolean hasMoreThanOne(){
-        return diceGroup.size() > 1;
-    }
-
-    public static void main(String[] args) {
-        //for pure testing
-        Dice d = new Dice(3);//creating a Dice group of three
-        System.out.println(d.getNexdtHighest());//print the highest
-        System.out.println(d.getNexdtHighest());
-        System.out.println(d.getNexdtHighest());
-        System.out.println(d.getNexdtHighest());//through Exception when no more dice in the queue
     }
 }
